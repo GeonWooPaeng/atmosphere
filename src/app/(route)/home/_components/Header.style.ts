69340bff0c93506420ab8934d0c07edc852300f4
GeonWooPaeng <geonwoopaeng@gmail.com>
2023-10-22 19:40:16 +0900

    feat: header style 구현 및 svg, font 추가

---
+++ src/app/(route)/home/_components/Header.style.ts
@@ -0,0 +1,35 @@
+'use client';
+import styled from 'styled-components';
+
+// header 전체
+export const Container = styled.div`
+  padding: 1.5rem 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+// 로고 부분
+export const Logo = styled.div`
+  display: flex;
+  gap: 0.7rem;
+
+  > span {
+    color: #49634e;
+    padding-bottom: 0.2rem;
+    font-size: 1.25rem;
+    font-weight: 700;
+    letter-spacing: 0.0625rem;
+    align-self: end;
+  }
+`;
+
+// user 부분
+export const User = styled.div`
+  padding: 0.5rem 0.5625rem 0.5rem 0.9375rem;
+  display: flex;
+  align-items: center;
+  gap: 0.875rem;
+  border-radius: 1.875rem;
+  border: 1px solid #ddd;
+`;
